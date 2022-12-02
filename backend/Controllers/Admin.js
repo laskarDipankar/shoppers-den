2408117be139efe88c537232c43a517584d3722b
laskarDipankar <dipankar.laskar.work@gmail.com>
2022-12-02 18:07:07 +0530

    Refactor

--- backend/Controllers/Admin.js
+++ backend/Controllers/Admin.js
@@ -1,39 +1,37 @@
-const Admin = require("../Model/Admin");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const Shop = require("../Model/Shop");
-const Users = require("../Model/User");
+const Admin = require('../Model/Admin');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Shop = require('../Model/Shop');
+const Users = require('../Model/User');
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "24h",
+    expiresIn: '24h',
   });
 };
 
 const admin = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
-  console.log(req.body);
-
   if (!firstName || !lastName || !email || !password) {
-    return res.status(422).send({ error: "please fill all the fields" });
+    return res.status(422).send({ error: 'please fill all the fields' });
   }
 
   try {
-    const userExist = await Admin.findOne({ email });
-    if (userExist) {
+    const adminExist = await Admin.findOne({ email });
+    if (adminExist) {
       return res.status(400).send({
-        // data: userExist,
         ok: false,
-        error: "email already exist",
+        error: 'email already exist',
       });
     }
+
     const hash = await bcrypt.hash(req.body.password, 10);
-    const user = new Admin({ firstName, lastName, email, password: hash });
-    await user.save();
-    res.status(201).send({ message: "user registered successfully" });
+    const admin = new Admin({ firstName, lastName, email, password: hash });
+    await admin.save();
+    res.status(201).send({ message: 'user registered successfully' });
   } catch (err) {
-    console.log(err);
+    res.status(500).send({ message: 'Something went wrong.' });
   }
 };
 
@@ -41,23 +39,22 @@ const loginAdmin = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await Admin.findOne({ email });
-    console.log(user);
     if (!user) {
-      return res.status(400).send({ error: "Invalid credentials" });
+      return res.status(400).send({ error: 'Invalid credentials' });
     }
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      console.log("password ");
-      return res.status(400).send({ error: "Invalid credentials" });
+      return res.status(400).send({ error: 'Invalid credentials' });
     }
-    console.log(user._id);
+
     const token = generateToken(user._id);
     res.status(200).send({
-      ok: "success",
+      ok: 'success',
       token,
     });
   } catch (err) {
-    console.log(err);
+    res.status(500).send({ message: 'Something went wrong.' });
   }
 };
 
@@ -66,25 +63,22 @@ const getAdmin = async (req, res) => {
     const admin = await Admin.find();
     res.status(200).send(admin);
   } catch (err) {
-    console.log(err);
+    res.status(500).send({ message: 'Something went wrong.' });
   }
 };
 
 const verifyShop = async (req, res) => {
-  console.log(req.admin);
-  console.log(req.body);
-
   const shopId = req.params.id;
   const verify = req.body.verify;
 
   if (!shopId) {
     return res.status(400).json({
-      error: "Shop id is required.",
+      error: 'Shop id is required.',
     });
   }
+
   try {
     const shop = await Shop.findById(shopId);
-
     if (!shop) return res.status(404).json({ error: "Shop doesn't exist." });
 
     shop.verified = verify;
@@ -92,34 +86,33 @@ const verifyShop = async (req, res) => {
 
     return res.status(200).json({ success: true, shop });
   } catch (err) {
-    console.log(err);
+    res.status(500).send({ message: 'Something went wrong.' });
   }
 };
 
 const deleteShop = async (req, res) => {
-  const shopid = req.params.id;
-  const admin = req.admin;
-  console.log(shopid);
+  const shopId = req.params.id;
 
-  if (!shopid)
+  if (!shopId)
     return res.status(400).json({
-      error: "Shop id is required.",
+      error: 'Shop id is required.',
     });
 
   try {
-    const shop = await Shop.findById(shopid);
+    const shop = await Shop.findById(shopId);
     if (!shop) return res.status(404).json({ error: "Shop doesn't exist." });
-    const userID = shop.userID;
-    await Users.findOneAndUpdate({ _id: userID }, { $unset: { shop: "" } });
 
+    await Users.findOneAndUpdate(
+      { _id: shop.userID },
+      { $unset: { shop: '' } }
+    );
     await shop.remove();
-
-    return res.status(200).json({ success: true, message: "Shop deleted." });
+    return res.status(200).json({ success: true, message: 'Shop deleted.' });
   } catch (error) {
-    console.log(error);
     return res.status(error.status || 500).json({ error: error.message });
   }
 };
+
 module.exports = {
   admin,
   getAdmin,
--- backend/MIddleware/userAuth.js
+++ backend/MIddleware/userAuth.js
@@ -1,35 +1,28 @@
-const jwt = require("jsonwebtoken");
-const Users = require("../Model/User");
+const jwt = require('jsonwebtoken');
+const Users = require('../Model/User');
 
 const userAuth = async (req, res, next) => {
   const bearer = req.headers.authorization;
-  // console.lo
-
-  console.log(bearer, "bearer");
 
   if (!bearer) {
-    return res.status(401).send({ error: "Unauthorized User jai mata di" });
+    return res.status(401).send({ error: 'Unauthorized User.' });
   } else {
     try {
-      console.log("here 1");
-      const userToken = bearer.split(" ")[1];
+      const userToken = bearer.split(' ')[1];
       const payload = jwt.verify(userToken, process.env.JWT_SECRET);
-      const user = await Users.findById(payload.id);
-      console.log("here 2");
 
+      const user = await Users.findById(payload.id);
       if (!user) return res.status(403).json({ error: "User doesn't exist." });
 
-      if (user.role !== "USER")
+      if (user.role !== 'USER')
         return res
           .status(403)
-          .json({ error: "You are not allowed to access this route." });
+          .json({ error: 'You are not allowed to access this route.' });
 
       req.user = user;
       next();
-      console.log("here 3");
     } catch (err) {
-      console.log(err);
-      res.status(401).send({ error: "Unauthorized" });
+      res.status(401).send({ error: 'Unauthorized' });
     }
   }
 };
